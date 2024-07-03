2855b0ce7df64145da511bc0dfe1ba4f993a8b9a
Ewo Jima <ll-EwoJima-ll@outlook.com>
2024-07-03 02:48:32 -0400

    sasadasd

--- Gradle Thing/JavaGradleTemplate/src/main/java/HiddenEngine/Main.java
+++ Gradle Thing/JavaGradleTemplate/src/main/java/HiddenEngine/Main.java
@@ -1,141 +1,40 @@
 package HiddenEngine;
 
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
+// Re inport the repository
 
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+    public static void main(String[] args) /*throws InterruptedException*/ {
+        //Window.Title = "The Hidden Engine";
+        Window.Size(300, 500);
+        //Window.Width = 300;
+        //Window.Height = 500;
+        //Window.SetRGB(200, 100, 50, 130);
 
-	// The window handle
-	private long window;
+        // Window.FrameLimit = 10;
+        //Window.HexadecimalColor = 0x123456;
 
-	public void run() {
-		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+        //Window.Type = "fullscreen";
+        new Window().Create();
+        ///new Window().run();
+        
+        Window.SetRGB(100, 150, 50, 200);
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //TimeUnit.SECONDS.sleep(3);
+        //new Window().ChangeScene();
 
-		init();
-		loop();
+        //double x;
+        //double y;
+        //long window = Window.Window;
+        //DoubleBuffer xx = Mouse.GetPosX(window, x, y);
+        //System.out.println(xx + " " + y);
 
-		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
-	}
-
-	private void init() {
-		// Setup an error callback. The default implementation
-		// will print the error message in System.err.
-		GLFWErrorCallback.createPrint(System.err).set();
-
-		// Initialize GLFW. Most GLFW functions will not work before doing this.
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Configure GLFW
-		glfwDefaultWindowHints(); // optional, the current window hints are already the default
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-		// Create the window
-		window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-		if ( window == NULL )
-			throw new RuntimeException("Failed to create the GLFW window");
-
-		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-		});
-
-		// Get the thread stack and push a new frame
-		try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1); // int*
-			IntBuffer pHeight = stack.mallocInt(1); // int*
-
-			// Get the window size passed to glfwCreateWindow
-			glfwGetWindowSize(window, pWidth, pHeight);
-
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-			// Center the window
-			glfwSetWindowPos(
-				window,
-				(vidmode.width() - pWidth.get(0)) / 2,
-				(vidmode.height() - pHeight.get(0)) / 2
-			);
-		} // the stack frame is popped automatically
-
-		// Make the OpenGL context current
-		glfwMakeContextCurrent(window);
-		// Enable v-sync
-		glfwSwapInterval(1);
-
-		// Make the window visible
-		glfwShowWindow(window);
-	}
-
-	private void loop() {
-		// This line is critical for LWJGL's interoperation with GLFW's
-		// OpenGL context, or any context that is managed externally.
-		// LWJGL detects the context that is current in the current thread,
-		// creates the GLCapabilities instance and makes the OpenGL
-		// bindings available for use.
-		GL.createCapabilities();
-
-		// Set the clear color
-		glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-
-		// Run the rendering loop until the user has attempted to close
-		// the window or has pressed the ESCAPE key.
-		while ( !glfwWindowShouldClose(window) ) {
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
-			glfwSwapBuffers(window); // swap the color buffers
-
-			// Poll for window events. The key callback above will only be
-			// invoked during this call.
-			glfwPollEvents();
-		}
-	}
-
-	public class OlangHandling {
-		void OLang() { // Orca Language (The Official Hidden OS Programming Language)
-
-		}
-
-		void OLTE() { // Orca Language Text Editor
-
-		}
-	}
-
-	public class HiddenOS {
-		int RenderBackground() {
-
-			return 0;
-		}
-		int RenderHomeScreenApps() {
-			
-
-			return 0;
-		}
-
-	}
-
-	public static void main(String[] args) {
-		new Main().run();
-		
-	}
+    }
 
 }
